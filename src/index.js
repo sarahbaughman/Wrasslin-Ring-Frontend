7133ffc5b26895127faa76926a3df0ecea9decae
Sarah Baughman <sarahbaughman92@gmail.com>
2023-06-19 14:22:17 -0400

    started

---
+++ src/index.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './components/App';
+
+ReactDOM.render(
+    <React.StrictMode>
+        <App />
+    </React.StrictMode>,
+    document.getElementById('root')
+  );
+
+
+//   import React from 'react';
+//   import ReactDOM from 'react-dom/client';
+//   import { BrowserRouter } from "react-router-dom";
+// //   import './index.css';
+//   import App from './components/App';
+  
+//   const root = ReactDOM.createRoot(document.getElementById('root'));
+//   root.render(
+//     <React.StrictMode>
+//       <BrowserRouter>
+//         <App />
+//       </BrowserRouter>
+//     </React.StrictMode>
+//   );
